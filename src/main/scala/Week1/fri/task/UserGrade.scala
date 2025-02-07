d8ecfc66ea6300b4f99cc81e3e59e3bad2190111
tudorsonycx <tudorsonycx@gmail.com>
2025-02-07 17:15:34 +0000

    add UserGrade for score input and grade calculation

---
+++ src/main/scala/Week1/fri/task/UserGrade.scala
@@ -0,0 +1,41 @@
+package week1.fri.task
+
+import scala.io.StdIn.readLine
+
+object UserGrade extends App {
+
+  private val subjects: List[String] = List("Math", "Science", "English")
+
+  private def getScoresMap(): Map[String, Int] = {
+    subjects.map(subject => {
+      val score: Int = readLine(s"Enter score for $subject: ").toInt match {
+        case s if s < 0 => 0
+        case s if s > 100 => 100
+        case s => s
+      }
+      (subject, score)
+    }).toMap
+  }
+
+  def getAverageScore(scores: Map[String, Int]): Int = {
+    val scoresValues = scores.values
+    scoresValues.sum / scoresValues.size
+  }
+
+  def getGrade(totalScore: Int): String = {
+    if (totalScore < 60) "E"
+    else if (totalScore < 70) "D"
+    else if (totalScore < 80) "C"
+    else if (totalScore < 90) "B"
+    else "A"
+  }
+
+  private val scores: Map[String, Int] = getScoresMap()
+  println(s"Your scores: $scores")
+
+  private val averageScore: Int = getAverageScore(scores)
+  println(s"Your average score is $averageScore")
+
+  private val grade: String = getGrade(averageScore)
+  println(s"Your grade is $grade")
+}
