8677fbc198383f23205b3e940999a4ccb7537288
tudorsonycx <tudorsonycx@gmail.com>
2025-02-10 18:38:38 +0000

    add Animal trait

---
+++ src/main/scala/week2/day1/task/mvp/Animal.scala
@@ -0,0 +1,15 @@
+package week2.day1.task.mvp
+
+trait Animal {
+  val name: String
+  // age in years
+  val age: Float
+
+  def eat(): Unit = println(s"$name is eating")
+
+  def sleep(): Unit = println(s"$name is sleeping")
+
+  def poop(): Unit = println(s"$name is pooping")
+
+  def move(): Unit = println(s"$name is moving")
+}
