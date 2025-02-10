930cccb5d62848f7662f2393ed8c0dade6800de2
tudorsonycx <tudorsonycx@gmail.com>
2025-02-10 12:23:54 +0000

    add Child and Adult classes with companion objects for instantiation

---
+++ src/main/scala/week2/day1/codealong/Adult.scala
@@ -0,0 +1,15 @@
+package week2.day1.codealong
+
+class FirstName(val firstName: String)
+
+class LastName(val lastName: String)
+
+class Adult(val firstName: FirstName, val lastName: LastName) {
+
+  val under18: Boolean = false
+}
+
+object Adult {
+  def apply(firstName: FirstName, lastName: LastName): Adult =
+    new Adult(firstName, lastName)
+}
---
+++ src/main/scala/week2/day1/codealong/FamilyLogic.scala
@@ -0,0 +1,22 @@
+package week2.day1.codealong
+
+object FamilyLogic extends App {
+
+  // Class instantiation with keyword 'new'
+  new Child("John")
+  val child1: Child = new Child("John-2")
+  val child2: Child = new Child("John-2")
+  println(child1 == child2) // false
+  println(child1.firstName)
+  println(child1.under18)
+  // println(child1.idPlease) - method idPlease cannot be accessed from outside
+  // the Child class
+
+  val adult1: Adult = new Adult(new FirstName("Tudor"), new LastName("Dura"))
+  // create an instance without 'new' if the Adult companion object has defined
+  // the apply method
+  val adult2: Adult = Adult(new FirstName("Tudor"), new LastName("Dura"))
+
+  println(adult1.firstName.firstName)
+  println(adult1.lastName.lastName)
+}
