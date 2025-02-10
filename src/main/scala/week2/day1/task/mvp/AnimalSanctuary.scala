b8544676a5d34c005f3b43be29b8aa925f73449a
tudorsonycx <tudorsonycx@gmail.com>
2025-02-10 20:23:37 +0000

    refactor AnimalSanctuary to use a list of animals and implement describeAllAnimals method to describe all animals based on their type

--- src/main/scala/week2/day1/task/mvp/AnimalSanctuary.scala
+++ src/main/scala/week2/day1/task/mvp/AnimalSanctuary.scala
@@ -1,33 +1,40 @@
 package week2.day1.task.mvp
 
 object AnimalSanctuary extends App {
-  private val bear1: Bear = Bear("Bruno", 10 * 365, Omnivore, "brown", isPregnant = false)
+  val animals: List[Animal] = List(
+    Bear("Bruno", 10 * 365, Omnivore, "brown", isPregnant = false),
+    Parrot("Polly", 12 * 365, Herbivore, "red", 25),
+    Mantis("Manty", 100, Carnivore, 6, 10)
+  )
 
-  bear1.describe()
-  bear1.eat()
-  bear1.sleep()
-  bear1.poop()
-  bear1.move()
-  bear1.giveBirth()
-  bear1.hunt()
-  bear1.hibernate()
-  println()
+  def feedAnimal(animal: Animal): Unit = animal.eat()
 
-  private val parrot1: Parrot = Parrot("Polly", 12 * 365, Herbivore, "red", 25)
+  def describeAllAnimals(animals: List[Animal]): Unit = animals.foreach(animal =>{
+    animal.describe()
+    animal.eat()
+    animal.sleep()
+    animal.poop()
+    animal.move()
+    animal match {
+      case bear: Bear => {
+        bear.giveBirth()
+        bear.hibernate()
+        bear.hunt()
+      }
+      case parrot: Parrot => {
+        parrot.fly()
+        parrot.layEggs()
+        parrot.talk()
+      }
+      case mantis: Mantis => {
+        mantis.fly()
+        mantis.layEggs()
+        mantis.molt()
+      }
+      case _ => println("Unknown animal")
+    }
+    println()
+  })
 
-  parrot1.describe()
-  parrot1.fly()
-  parrot1.layEggs()
-  parrot1.talk()
-  parrot1.fly()
-  parrot1.sleep()
-  println()
-
-  private val mantis1: Mantis = Mantis("Manty", 100, Carnivore, 6, 10)
-
-  mantis1.describe()
-  mantis1.fly()
-  mantis1.molt()
-  mantis1.eat()
-  mantis1.sleep()
+  describeAllAnimals(animals)
 }
