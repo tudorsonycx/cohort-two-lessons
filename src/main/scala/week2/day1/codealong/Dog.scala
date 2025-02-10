b9645f62d700eb27c061517dcef213107a99bb84
tudorsonycx <tudorsonycx@gmail.com>
2025-02-10 12:42:45 +0000

    add Dog class with properties and methods, and DogLogic object for instantiation

---
+++ src/main/scala/week2/day1/codealong/Dog.scala
@@ -0,0 +1,15 @@
+package week2.day1.codealong
+
+class Dog(val name: String, val age: Int, val breed: String, val isBig: Boolean) {
+
+  def bark(): Unit = println(s"$name is barking!")
+
+  def bigBoi: String =
+    if (isBig) s"$name is a big boi!" else s"$name is not a very big boi!"
+}
+
+object Dog {
+
+  def apply(name: String, age: Int, breed: String, isBig: Boolean): Dog =
+    new Dog(name, age, breed, isBig)
+}
