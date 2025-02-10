a1d274ff80e4d945bf49ffac3dbfead6c21a2ebc
tudorsonycx <tudorsonycx@gmail.com>
2025-02-10 10:57:21 +0000

    add Object examples in Objects file

---
+++ src/main/scala/week2/day1/codealong/Objects.scala
@@ -0,0 +1,14 @@
+package week2.day1.codealong
+
+object Objects extends App {
+
+  object PrimaryColours {
+    val blue: String = "blue"
+    val red: String = "red"
+    val yellow: String = "yellow"
+  }
+
+  println(PrimaryColours.red)
+  println(PrimaryColours.blue)
+  println(PrimaryColours.yellow)
+}
