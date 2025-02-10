dccbb61386636f7f02036cc06ddb1cc106a00481
tudorsonycx <tudorsonycx@gmail.com>
2025-02-10 19:47:21 +0000

    add dietType member to Animal trait

--- src/main/scala/week2/day1/task/mvp/Animal.scala
+++ src/main/scala/week2/day1/task/mvp/Animal.scala
@@ -5,9 +5,13 @@ trait Animal extends Species {
   // age in days
   val age: Int
 
+  val dietType: DietType
+
   def printName(): Unit = println(s"Name: $name")
 
   def printAge(): Unit = println(f"Age: ${age / 365f}%.2f years")
+
+  def printDietType(): Unit = println(s"Diet type: $dietType")
 
   def eat(): Unit = println(s"$name is eating")
 
