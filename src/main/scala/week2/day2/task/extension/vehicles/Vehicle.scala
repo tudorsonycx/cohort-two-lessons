547441dc3c4c98accba1c61f442e7b6ac4e2d8b2
tudorsonycx <tudorsonycx@gmail.com>
2025-02-11 18:19:31 +0000

    refactor: convert Vehicle class to trait and enhance functionality with new methods

--- src/main/scala/week2/day2/task/extension/vehicles/Vehicle.scala
+++ src/main/scala/week2/day2/task/extension/vehicles/Vehicle.scala
@@ -1,7 +1,23 @@
 package week2.day2.task.extension.vehicles
 
-abstract class Vehicle(val make: String) {
-  val description: String = s"This vehicle is a $make"
+trait Vehicle {
+  val make: String
+  val model: String
+  val year: Int
+  val color: String
+  val description: String = s"This vehicle is a $make $model from $year and it is $color"
 
-  def drive: String
+  def startEngine: String = s"The $make $model is starting"
+
+  def stopEngine: String = s"The $make $model is stopping"
+
+  def accelerate: String = s"The $make $model is accelerating"
+
+  def brake: String = s"The $make $model is braking"
+
+  def turnLeft: String = s"The $make $model is turning left"
+
+  def turnRight: String = s"The $make $model is turning right"
+
+  def reverse: String = s"The $make $model is reversing"
 }
