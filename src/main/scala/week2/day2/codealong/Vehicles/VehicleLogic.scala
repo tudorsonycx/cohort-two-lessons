6f42a8c3c28f8113966460a2c7b03f223e599532
tudorsonycx <tudorsonycx@gmail.com>
2025-02-11 12:59:53 +0000

    add VehicleLogic object to demonstrate vehicle functionality with Car and ElectricCar

---
+++ src/main/scala/week2/day2/codealong/Vehicles/VehicleLogic.scala
@@ -0,0 +1,14 @@
+package week2.day2.codealong.Vehicles
+
+object VehicleLogic extends App {
+  val car1: Car = new Car("Toyota", "Camry", "diesel")
+  println(car1.description)
+  println(car1.drive)
+  println()
+
+  val electricCar1: ElectricCar = ElectricCar("Tesla", "Model S", 5)
+  println(electricCar1)
+  println(electricCar1.timeToCharge)
+  println(electricCar1.charge)
+  println(electricCar1.drive)
+}
