0eb481149688b6fb6cf241361a9f3894b697f7a5
tudorsonycx <tudorsonycx@gmail.com>
2025-02-20 15:48:50 +0000

    add Animal trait with implementations and facts for various animals

--- src/main/scala/week3/day4/tasks/MVP.scala
+++ src/main/scala/week3/day4/tasks/MVP.scala
@@ -21,5 +21,28 @@ object MVP extends App {
     List("   EnglanD", "fraNCe  ", "  Germany", "Italy", "spain", " portugal", "  NetherLANDS  ", " belgiuM", "Switzerland  ", "AUSTRIA", "Russia")
 
   countries.foreach(country => println(s"Capital of $country is ${getCapital(country)}"))
+  println()
 
+  sealed trait Animal
+
+  case object Dog extends Animal
+
+  case object Cat extends Animal
+
+  case object Bird extends Animal
+
+  case object Fish extends Animal
+
+  def getAnimalFact(animal: Animal): String = {
+    animal match {
+      case Dog => "Dogs can smell about 1,000 times better than humans."
+      case Cat => "Cats have five toes on their front paws, but only four on their back paws."
+      case Bird => "The smallest bird in the world is the bee hummingbird."
+      case Fish => "Fish can see in color."
+    }
+  }
+
+  val animals: List[Animal] = List(Dog, Cat, Bird, Fish)
+
+  animals.foreach(animal => println(getAnimalFact(animal)))
 }
