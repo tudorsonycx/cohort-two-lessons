cbc0b0fa29e75229e9ab31b3aaf7645fd63282ca
tudorsonycx <tudorsonycx@gmail.com>
2025-02-20 19:52:27 +0000

    add Person case class with creation logic and input handling

--- src/main/scala/week3/day4/tasks/MVP.scala
+++ src/main/scala/week3/day4/tasks/MVP.scala
@@ -1,5 +1,7 @@
 package week3.day4.tasks
 
+import java.time.LocalDate
+
 object MVP extends App {
   def getCapital(country: String): String = {
     country.trim.toLowerCase match {
@@ -45,4 +47,75 @@ object MVP extends App {
   val animals: List[Animal] = List(Dog, Cat, Bird, Fish)
 
   animals.foreach(animal => println(getAnimalFact(animal)))
+
+  case class Person private(
+    fullName: String,
+    dateOfBirth: LocalDate,
+    occupation: Option[String],
+    lastCountryLivedIn: String,
+    dateOfDeath: Option[LocalDate],
+    age: Int,
+    starSign: String,
+    ageAtDeath: Option[Int]
+  )
+
+  case class InputPerson(
+    fullName: String,
+    dateOfBirth: LocalDate,
+    occupation: Option[String],
+    lastCountryLivedIn: String,
+    dateOfDeath: Option[LocalDate]
+  )
+
+  object Person {
+    def createPerson(inputPerson: InputPerson): Person = {
+      val age: Int = inputPerson.dateOfBirth.until(LocalDate.now()).getYears
+
+      val month: Int = inputPerson.dateOfBirth.getMonthValue
+      val day: Int = inputPerson.dateOfBirth.getDayOfMonth
+      val starSign: String = {
+        (month, day) match {
+          case (m, d) if (m == 3 && d > 20) || (m == 4 && d < 20) => "Aries"
+          case (m, d) if (m == 4 && d > 19) || (m == 5 && d < 21) => "Taurus"
+          case (m, d) if (m == 5 && d > 20) || (m == 6 && d < 22) => "Gemini"
+          case (m, d) if (m == 6 && d > 21) || (m == 7 && d < 23) => "Cancer"
+          case (m, d) if (m == 7 && d > 22) || (m == 8 && d < 23) => "Leo"
+          case (m, d) if (m == 8 && d > 22) || (m == 9 && d < 23) => "Virgo"
+          case (m, d) if (m == 9 && d > 22) || (m == 10 && d < 23) => "Libra"
+          case (m, d) if (m == 10 && d > 23) || (m == 11 && d < 22) => "Scorpio"
+          case (m, d) if (m == 11 && d > 21) || (m == 12 && d < 22) => "Sagittarius"
+          case (m, d) if (m == 12 && d > 21) || (m == 1 && d < 20) => "Capricorn"
+          case (m, d) if (m == 1 && d > 19) || (m == 2 && d < 19) => "Aquarius"
+          case (m, d) if (m == 2 && d > 18) || (m == 3 && d < 21) => "Pisces"
+        }
+      }
+
+      val ageAtDeath: Option[Int] =
+        inputPerson.dateOfDeath match {
+          case Some(value) => Some(inputPerson.dateOfBirth.until(value).getYears)
+          case None => None
+        }
+
+      Person(
+        inputPerson.fullName,
+        inputPerson.dateOfBirth,
+        inputPerson.occupation,
+        inputPerson.lastCountryLivedIn,
+        inputPerson.dateOfDeath,
+        age,
+        starSign,
+        ageAtDeath
+      )
+    }
+  }
+
+  val inputPerson: InputPerson = InputPerson(
+    "John Doe",
+    LocalDate.of(1990, 4, 1),
+    Some("Software Engineer"),
+    "England",
+    Some(LocalDate.of(2025, 1, 1))
+  )
+  println(Person.createPerson(inputPerson))
+
 }
