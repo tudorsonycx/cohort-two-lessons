9ca8e15ed8c9c090eea91d225e579f260f5af8cf
tudorsonycx <tudorsonycx@gmail.com>
2025-02-21 20:31:31 +0000

    add validation methods for Name and Postcode, and create Letter object with error handling

--- src/main/scala/week3/day5/Tasks/Letter.scala
+++ src/main/scala/week3/day5/Tasks/Letter.scala
@@ -1,13 +1,45 @@
 package week3.day5.Tasks
 
+import java.lang.classfile.ClassFile.Option
+
 case class Name(value: String)
 
+object Name {
+  def nameOrError(maybeName: String): Either[InvalidNameError, Name] = {
+    maybeName match {
+      case s if s.exists(_.isDigit) => Left(InvalidNameError("Name contains digit"))
+      case s => Right(Name(s))
+    }
+  }
+}
+
 case class Postcode(value: String)
+
+object Postcode {
+  def postcodeOrError(maybePostCode: String): Either[InvalidPostcodeError, Postcode] = {
+    maybePostCode match {
+      case s if s.trim != s || s.count(_ == ' ') != 1 => Left(InvalidPostcodeError("Invalid postcode"))
+      case s => Right(Postcode(s))
+    }
+  }
+}
 
 case class Letter(name: Name, postcode: Postcode)
 
 trait GenericPostageError {
   val message: String
+}
+
+object Letter {
+  def letterOrError(maybeName: String, maybePostCode: String): Either[GenericPostageError, Letter] = {
+    val postcode: Either[InvalidPostcodeError, Postcode] = Postcode.postcodeOrError(maybePostCode)
+    val name: Either[InvalidNameError, Name] = Name.nameOrError(maybeName)
+    postcode.flatMap(postcode => {
+      name.map(name => {
+        Letter(name, postcode)
+      })
+    })
+  }
 }
 
 case class InvalidNameError(message: String) extends GenericPostageError
