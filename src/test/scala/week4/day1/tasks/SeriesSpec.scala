44e00961d5fbb51a721e10b4a7a129c7ff21c709
tudorsonycx <tudorsonycx@gmail.com>
2025-02-24 16:52:55 +0000

    refactor SeriesError to accept a message and add isEven method for list index checks

--- src/test/scala/week4/day1/tasks/SeriesSpec.scala
+++ src/test/scala/week4/day1/tasks/SeriesSpec.scala
@@ -8,7 +8,7 @@ class SeriesSpec extends AnyWordSpec with Matchers {
   "Series.addN" should {
     "return Left(SeriesError)" when {
       "input is negative" in {
-        Series.addN(-10) shouldBe Left(SeriesError())
+        Series.addN(-10) shouldBe Left(SeriesError("Input should be positive"))
       }
     }
 
@@ -24,6 +24,35 @@ class SeriesSpec extends AnyWordSpec with Matchers {
         Series.addN(10) shouldBe Right(55)
         Series.addN(123) shouldBe Right(7626)
         Series.addN(123456) shouldBe Right(7620753696L)
+      }
+    }
+  }
+
+  val list: List[Int] = (1 to 20).toList
+
+  "Series.isEven" should {
+    "return Left(SeriesError)" when {
+      "input list ins empty" in {
+        Series.isEven(List(), 1) shouldBe Left(SeriesError("List is empty"))
+      }
+
+      "index is out of bounds" in {
+        Series.isEven(list, -1) shouldBe Left(SeriesError("Index is out of bounds"))
+        Series.isEven(list, 21) shouldBe Left(SeriesError("Index is out of bounds"))
+      }
+    }
+
+    "return Right(true)" when {
+      "element at input index is even" in {
+        Series.isEven(list, 1) shouldBe Right(true)
+        Series.isEven(list, 19) shouldBe Right(true)
+      }
+    }
+
+    "return Right(false)" when {
+      "element at input index is odd" in {
+        Series.isEven(list, 0) shouldBe Right(false)
+        Series.isEven(list, 18) shouldBe Right(false)
       }
     }
   }
